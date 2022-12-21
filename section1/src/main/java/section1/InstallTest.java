5f67b9f25747c23dc414142036f147ee4dba3171
Matheus Araujo Souza <mhatheussss@gmail.com>
2022-12-21 08:51:26 -0300

    section 1 apache beam course

---
+++ section1/src/main/java/section1/InstallTest.java
@@ -0,0 +1,14 @@
+package section1;
+
+import org.apache.beam.sdk.Pipeline;
+
+public class InstallTest {
+	
+	public static void main(String[] args) {
+		Pipeline pipe = Pipeline.create();
+		
+		System.out.println("Done");
+		
+	}
+
+}
